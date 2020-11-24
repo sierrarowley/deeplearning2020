d75a2a4c197d72c74a4be67363cb54e1cb054e8c
sierrarowley <60280017+sierrarowley@users.noreply.github.com>
2020-11-23 21:33:42 -0500

    train test split

--- model.py
+++ model.py
@@ -5,15 +5,12 @@ from preprocess import get_data
 
 def main():
     data_ids, labels, vocab_dict = get_data()
-    print(data_ids)
-    print(labels)
-    print(data_ids.shape, labels.shape)
 
     # 85% is training data, 15% is test data
     train_ids = data_ids[:470, :]
-    train_labels = [:470]
-    test_ids = [470:, :]
-    test_labels = [470:]
+    train_labels = labels[:470]
+    test_ids = data_ids[470:, :]
+    test_labels = labels[470:]
 
 if __name__ == "__main__":
     main()
